1cb62cf146b8d7a0dc633a7abdb740835008704f
mfpang <pangminfu89@gmail.com>
2023-02-05 22:47:38 +0800

    chore: added golangci lint and fix linting issue

--- internal/service/todo.go
+++ internal/service/todo.go
@@ -1,10 +1,11 @@
 package service
 
 import (
-	"backend-practices/internal/domain/todo"
-	"backend-practices/internal/repository"
 	"context"
 	"fmt"
+
+	"backend-practices/internal/domain/todo"
+	"backend-practices/internal/repository"
 )
 
 var _ TodoServicer = (*todoService)(nil)
@@ -20,18 +21,18 @@ func NewTodo(repo repository.TodoRepo) *todoService {
 }
 
 func (s *todoService) Create(ctx context.Context, req todo.CreateReq) error {
-	todo := &todo.Todo{
+	todoList := &todo.Todo{
 		User:        req.User,
 		Title:       req.Title,
 		Description: req.Description,
 		Items:       req.Items,
 	}
 
-	if err := s.repo.Create(ctx, todo); err != nil {
+	if err := s.repo.Create(ctx, todoList); err != nil {
 		return fmt.Errorf("todo Create(): %w", err)
 	}
 
-	if err := s.repo.CreteItems(ctx, todo.Items); err != nil {
+	if err := s.repo.CreteItems(ctx, todoList.Items); err != nil {
 		return fmt.Errorf("todo CreateItems(): %w", err)
 	}
 
